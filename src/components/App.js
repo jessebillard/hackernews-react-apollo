0c93f54dc644a81fe3834e95d399c091b5a55e6e
jessebillard <jessebillard@mac.com>
2018-08-27 11:02:43 -0400

    adds CreateLink input for GraphQL post mutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LinkList />
+        <CreateLink></CreateLink>
       </div>
     );
   }
