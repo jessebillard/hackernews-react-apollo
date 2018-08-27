29ecac86ae621946bef2d77d17f24d6bff5a4a18
jessebillard <jessebillard@mac.com>
2018-08-27 15:22:50 -0400

    establishes websocket connection

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
@@ -25,10 +28,29 @@ const authLink = setContext((_, { headers }) => {
     }
   })
 
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+  const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: localStorage.getItem(AUTH_TOKEN),
+      }
+    }
+  })
+  
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+  )
+  
+  const client = new ApolloClient({
+    link,
     cache: new InMemoryCache()
-})
+  })
 
 ReactDOM.render(
     <BrowserRouter>
